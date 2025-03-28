1ed6eed725ed3fc00b255adca4386bdbdf79cf64
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2025-03-28 18:02:44 +0100

    feat: add validation rules to the Customer class field

--- src/main/java/com/springboot/mvc/model/Customer.java
+++ src/main/java/com/springboot/mvc/model/Customer.java
@@ -1,15 +1,20 @@
 package com.springboot.mvc.model;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @ToString
 public class Customer {
+
+    @NotNull(message = "is required")
+    @Size(min=1, message = "is required")
     private String firstName;
+
+    @NotNull(message = "is required")
+    @Size(min=1, message = "is required")
     private String lastName;
 }
