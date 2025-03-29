8ffec78651b4294652525b656e9a2b84683d98b2
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2025-03-29 18:06:49 +0100

    feat: perform the required fields validation for the CustomerController

--- src/main/java/com/springboot/mvc/controller/CustomerController.java
+++ src/main/java/com/springboot/mvc/controller/CustomerController.java
@@ -1,8 +1,10 @@
 package com.springboot.mvc.controller;
 
 import com.springboot.mvc.model.Customer;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,15 +12,26 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class CustomerController {
     @GetMapping("/customer-form")
-    public String showCustomerForm(Model theModel){
+    public String showCustomerForm(Model theModel) {
         theModel.addAttribute("customer", new Customer());
         return "customer-form";
     }
 
     @PostMapping("/processed-customer-form")
-    public String processCustomerForm(@ModelAttribute("customer") Customer theCustomer){
+    // @Valid corresponds to validation rules in the Customer class and tells
+    // Spring MVC to perform validation
+    // theBindingResult holds the validation result
+    public String processCustomerForm(@Valid @ModelAttribute("customer") Customer theCustomer,
+                                      BindingResult theBindingResult) {
         System.out.println(theCustomer);
-        return "processed-customer-form";
+        System.out.println(theBindingResult);
+
+        if(theBindingResult.hasErrors()){
+            return "customer-form";
+        }else{
+            return "processed-customer-form";
+        }
+
     }
 
 }
